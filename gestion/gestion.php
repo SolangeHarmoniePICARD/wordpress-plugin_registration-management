16d4fea2df686d2e998221c4b38fc444d7237520
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-08-24 22:50:54 +0200

    Déclaration du plugin.

---
+++ gestion/gestion.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Gestion des inscriptions
+Plugin URI: https://solangeharmoniepicard.fr/
+Description: Un plugin de gestion des inscriptions à un évènement.
+Author: Solange Harmonie PICARD
+Version: 1.0
+Author URI: https://solangeharmoniepicard.fr/
+*/
